f4e2ada07f6a39f9b5cd2fe22481fbbf7859f6e7
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-02-23 01:41:12 +0200

    m

--- controllers/index.js
+++ controllers/index.js
@@ -73,7 +73,7 @@ router.get('/invite', function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      list = list.filter(x => x.confirmed == true && x.programmer == undefined && x.accepted == true && x.invited != true && x.workshopMail === true);  /* Accept */
+      list = list.filter(x => x.confirmed == true && x.programmer == undefined && x.accepted == true && x.invited != true && x.workshopMail === true).slice(0, 15);  /* Accept */
       list.map(doc => {
         ejs.renderFile(__dirname + '/../views/invitation.ejs', {
           code: doc._id,
